2c81da540267e8ed3d83e93388eb22e08aba90ec
Favre Anael <anael.favre@gmail.com>
2017-03-16 17:37:28 +0100

    Laravel 5.3 compat

--- src/Validator.php
+++ src/Validator.php
@@ -8,29 +8,30 @@ use Illuminate\Validation\Validator as IlluminateValidator;
  */
 class Validator extends IlluminateValidator
 {
-
 	/**
 	 * @param $attribute
 	 * @param $value
 	 * @param $parameters
+	 *
 	 * @return bool
 	 */
 	public function validateMayIn($attribute, $value, $parameters)
 	{
 		return true;
 	}
-
+	
 	/**
 	 * @param $attribute
 	 * @param $value
 	 * @param $parameters
+	 *
 	 * @return bool
 	 */
 	public function validateHidden($attribute, $value, $parameters)
 	{
 		return true;
 	}
-
+	
 	/**
 	 * @param $value
 	 *
@@ -39,16 +40,16 @@ class Validator extends IlluminateValidator
 	public function validateNumberInterval($attribute, $value, $parameters)
 	{
 		return true;
-
+		
 		$value = trim($value);
-
-		if (!preg_match('#^[<>][0-9]*$#', $value)) {
+		
+		if (! preg_match('#^[<>][0-9]*$#', $value)) {
 			return false;
 		}
-
+		
 		return true;
 	}
-
+	
 	/**
 	 * @param $attribute
 	 * @param $value
@@ -60,19 +61,35 @@ class Validator extends IlluminateValidator
 	{
 		return preg_match('#^0[1-9]([-. ]?[0-9]{2}){4}$#', $value);
 	}
-
+	
+	/**
+	 * @param $attribute
+	 * @param $value
+	 * @param $parameters
+	 *
+	 * @return bool
+	 */
 	public function validateDepartment($attribute, $value, $parameters)
 	{
-		$departments = json_decode(file_get_contents('http://geo.api.web-6.fr/departments?search[id]=' . $value), true);
+		$departments = json_decode(file_get_contents('http://geo.api.web-6.fr/departments?search[id]='.$value), true);
+		
 		return count($departments) > 0;
 	}
-
+	
+	/**
+	 * @param $attribute
+	 * @param $value
+	 * @param $parameters
+	 *
+	 * @return bool
+	 */
 	public function validateCity($attribute, $value, $parameters)
 	{
-		$cities = json_decode(file_get_contents('http://geo.api.web-6.fr/cities?search[id]=' . $value), true);
+		$cities = json_decode(file_get_contents('http://geo.api.web-6.fr/cities?search[id]='.$value), true);
+		
 		return count($cities) > 0;
 	}
-
+	
 	/**
 	 * @param $attribute
 	 * @param $value
@@ -85,32 +102,18 @@ class Validator extends IlluminateValidator
 		// Transform the each rule
 		// For example, `each:exists,users,name` becomes `exists:users,name`
 		$ruleName = array_shift($parameters);
-		$rule = $ruleName . (count($parameters) > 0 ? ':' . implode(',', $parameters) : '');
-
+		$rule = $ruleName.(count($parameters) > 0 ? ':'.implode(',', $parameters) : '');
+		
 		foreach ($value as $arrayKey => $arrayValue) {
-			$this->validate($attribute . '.' . $arrayKey, $rule);
+			if(method_exists($this, 'validateAttribute')) {
+				$this->validateAttribute($attribute.'.'.$arrayKey, $rule);
+			}
+			else {
+				$this->validate($attribute.'.'.$arrayKey, $rule);
+			}
 		}
-
+		
 		// Always return true, since the errors occur for individual elements.
 		return true;
-	}
-
-	/**
-	 * Get the displayable name of the attribute.
-	 *
-	 * @param  string $attribute
-	 * @return string
-	 */
-	protected function getAttribute($attribute)
-	{
-		// Get the second to last segment in singular form for arrays.
-		// For example, `group.names.0` becomes `name`.
-		if (str_contains($attribute, '.')) {
-			$segments = explode('.', $attribute);
-
-			$attribute = str_singular($segments[count($segments) - 2]);
-		}
-
-		return parent::getAttribute($attribute);
 	}
 }
